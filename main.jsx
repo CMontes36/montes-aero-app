80031ee6515e2dc4ae8fa308beca547ae2186b71
CMontes36 <support@montesaero.com>
2026-04-06 23:02:15 -0600

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -320,3 +320,16 @@ const App = () => {
 };
 
 export default App;
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Montes Aero</title>
+    <script src="[https://cdn.tailwindcss.com](https://cdn.tailwindcss.com)"></script>
+  </head>
+  <body>
+    <div id="root"></div>
+    <script type="module" src="/main.jsx"></script>
+  </body>
+</html>
